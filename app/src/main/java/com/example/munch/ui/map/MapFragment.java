1890910cfd56ed570ea84f523182d4360461a65d
yasirayounus <32212118+yasirayounus@users.noreply.github.com>
2019-10-12 17:24:12 -0500

    Got map fragment to work, and added initial sliding panel

--- app/src/main/java/com/example/munch/ui/map/MapFragment.java
+++ app/src/main/java/com/example/munch/ui/map/MapFragment.java
@@ -10,8 +10,14 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.munch.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment{
 
     private MapViewModel mapViewModel;
 
@@ -20,13 +26,27 @@ public class MapFragment extends Fragment {
         mapViewModel =
                 ViewModelProviders.of(this).get(MapViewModel.class);
         View root = inflater.inflate(R.layout.fragment_map, container, false);
-//        final TextView textView = root.findViewById(R.id.text_map);
-//        mapViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                mMap.clear(); //clear old markers
+
+                CameraPosition austin = CameraPosition.builder()
+                        .target(new LatLng(30.267153, -97.743057))
+                        .zoom(10)
+                        .bearing(0)
+                        .tilt(45)
+                        .build();
+
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(austin), 10500, null);
+            }
+        });
+
+
         return root;
     }
+
 }
