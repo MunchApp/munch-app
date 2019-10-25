fc399dbc22b790ee6eb379f8141e7bcc24951713
yasirayounus <32212118+yasirayounus@users.noreply.github.com>
2019-10-25 17:42:36 -0500

    made quick changes to fix errors

--- app/src/main/java/com/example/munch/data/model/Review.java
+++ app/src/main/java/com/example/munch/data/model/Review.java
@@ -1,14 +1,12 @@
 package com.example.munch.data.model;
 
-import java.util.Date;
-
 public class Review {
 
-    String reviewer;
-    Date date;
-    FoodTruck truck;
-    Float rating;
-    String comment;
+//    String reviewer;
+//    Date date;
+//    FoodTruck truck;
+//    Float rating;
+//    String comment;
     //Photos
 
     public Review(){
