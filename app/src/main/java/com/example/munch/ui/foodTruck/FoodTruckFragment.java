133e4f27f68c2cc9c11b9c2b1717b8d6309d744b
Majjalpee <mazharnaqvi786@gmail.com>
2019-10-25 23:01:49 -0500

    done with reviews

--- app/src/main/java/com/example/munch/ui/foodTruck/FoodTruckFragment.java
+++ app/src/main/java/com/example/munch/ui/foodTruck/FoodTruckFragment.java
@@ -1,25 +1,18 @@
 package com.example.munch.ui.foodTruck;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.example.munch.R;
-import com.example.munch.ReviewListing;
-import com.example.munch.ReviewListingAdapter;
-import com.example.munch.SearchListing;
-import com.example.munch.SearchListingAdapter;
-import com.example.munch.data.model.FoodTruck;
-import com.example.munch.ui.map.MapViewModel;
-
-import java.util.ArrayList;
+import com.example.munch.ui.Reviewspageactivity;
 
 public class FoodTruckFragment extends Fragment {
 
@@ -30,21 +23,21 @@ public class FoodTruckFragment extends Fragment {
         foodTruckViewModel =
                 ViewModelProviders.of(this).get(FoodTruckViewModel.class);
         View root = inflater.inflate(R.layout.food_truck_activity, container, false);
-        populateReviews(root);
+        makeReviewClickable(root);
         return root;
     }
 
-    private void populateReviews(View root) {
-        ListView resultsList = (ListView) root.findViewById(R.id.review_layout);
-        ArrayList<ReviewListing> listings = new ArrayList<>();
-        //TODO Make Dynamic
-
-        listings.add(new ReviewListing("SYED NAQVI", "October 25,2019",
-                "This is a review! this food truck is so amazing! Please let me see this food truck!\nI want to eat here so bad! Nevermind I hate this place!\nOK just kidding, this place is awesome please come eat here!",4.9));
-        listings.add(new ReviewListing("JANINE BARIUAN", "October 25,2019",
-                "This is a review! this food truck is so amazing! Please let me see this food truck!\nI want to eat here so bad! Nevermind I hate this place!\nOK just kidding, this place is awesome please come eat here!",4.9));
-        ReviewListingAdapter adapter = new ReviewListingAdapter(getActivity(), listings);
-        resultsList.setAdapter(adapter);
-
+    private void makeReviewClickable(View root) {
+        TextView reviews = (TextView) root.findViewById(R.id.review_text);
+        reviews.setClickable(true);
+        reviews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Reviewspageactivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
