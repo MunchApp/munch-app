71fcbfc7749eeac0e56937b1ba303decf50ab637
yasirayounus <32212118+yasirayounus@users.noreply.github.com>
2019-11-06 11:50:13 -0600

    get and post reviews working

---
+++ app/src/main/java/com/example/munch/ui/AddReviewActivity.java
@@ -0,0 +1,43 @@
+package com.example.munch.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.munch.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class AddReviewActivity extends AppCompatActivity {
+
+    Button mSubmitReviewBtn;
+    TextInputLayout mReviewContent;
+    RatingBar mReviewRating;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
+        mReviewRating = findViewById(R.id.ratingBarReview);
+        mReviewContent = findViewById(R.id.reviewContent);
+        mSubmitReviewBtn = findViewById(R.id.submitNewReviewBtn);
+        mSubmitReviewBtn.setOnClickListener(           //action triggered on button click
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+
+                        String content = mReviewContent.getEditText().getText().toString();
+                        String rating = String.valueOf(mReviewRating.getRating());
+                        Intent intent = new Intent();
+                        intent.putExtra("content", content);
+                        intent.putExtra("rating", rating);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                });
+
+    }
+
+}
--- app/src/main/java/com/example/munch/ui/Reviewspageactivity.java
+++ app/src/main/java/com/example/munch/ui/Reviewspageactivity.java
@@ -1,38 +1,134 @@
 package com.example.munch.ui;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.munch.HttpRequests;
 import com.example.munch.R;
 import com.example.munch.ReviewListing;
 import com.example.munch.ReviewListingAdapter;
+import com.example.munch.ui.userProfile.UserProfileFragment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class Reviewspageactivity extends AppCompatActivity {
+
+    Button mAddReviewBtn;
+    ListView resultsList;
+    ArrayList<ReviewListing> listings;
+    ReviewListingAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviewspageactivity);
         populateReviews(findViewById(R.id.review_page));
+        mAddReviewBtn = findViewById(R.id.reviewButton);
+        if(!UserProfileFragment.currentUser.getLoggedIn()){
+            mAddReviewBtn.setEnabled(false);
+        } else {
+            mAddReviewBtn.setEnabled(true);
+            mAddReviewBtn.setOnClickListener(           //action triggered on button click
+                    new View.OnClickListener() {
+                        public void onClick(View view) {
+
+                            Intent addReviewIntent = new Intent(Reviewspageactivity.this, AddReviewActivity.class);
+                            startActivityForResult(addReviewIntent, 1);
+                        }
+                    });
+        }
+
     }
 
     private void populateReviews(View root) {
-        ListView resultsList = (ListView) root.findViewById(R.id.review_layout);
-        ArrayList<ReviewListing> listings = new ArrayList<>();
-        //TODO Make Dynamic
+        resultsList = (ListView) root.findViewById(R.id.review_layout);
+        listings = new ArrayList<>();
 
-        listings.add(new ReviewListing("SYED NAQVI", "October 25,2019",
-                "This is a review! this food truck is so amazing! Please let me see this food truck!\nI want to eat here so bad! Nevermind I hate this place!\nOK just kidding, this place is awesome please come eat here!",4.9));
-        listings.add(new ReviewListing("JANINE BARIUAN", "October 25,2019",
-                "This is a review! this food truck is so amazing! Please let me see this food truck!\nI want to eat here so bad! Nevermind I hate this place!\nOK just kidding, this place is awesome please come eat here!",4.9));
-        ReviewListingAdapter adapter = new ReviewListingAdapter(this, listings);
+
+        HttpRequests reviewRequest = new HttpRequests();
+        reviewRequest.execute("reviews", "GET");
+        String responseReview = null;
+        try {
+            responseReview = reviewRequest.get();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            JSONArray reviewData = new JSONArray(responseReview);
+            for (int i = 0; i < reviewData.length(); i++) {
+                JSONObject jsonobject = reviewData.getJSONObject(i);
+                String author = jsonobject.getString("reviewer");
+                String date = jsonobject.getString("date");
+                String reviewBody = jsonobject.getString("comment");
+                Double rating = jsonobject.getDouble("rating");
+                listings.add(new ReviewListing(author, date, reviewBody, rating));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        adapter = new ReviewListingAdapter(this, listings);
         resultsList.setAdapter(adapter);
 
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Reviewspageactivity.this.invalidateOptionsMenu();
+        if (resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+//                ReviewListing newReview = new ReviewListing("anon", (new Date().toString()), data.getStringExtra("content"), Double.valueOf(data.getStringExtra("rating")));
+//                listings.add(newReview);
+//                adapter.add(newReview);
+//                adapter.notifyDataSetChanged();
+                JSONObject review = new JSONObject();
+                try {
+                    review.put("foodTruck", "6f5c6b83-3df3-46a4-8408-f0d01eb6ed48");
+                    review.put("comment", data.getStringExtra("content"));
+                    review.put("rating", Float.valueOf(data.getStringExtra("rating")));
+                } catch (JSONException ex) {
+                    System.out.println("Post Review Failed");
+                }
+                String token = UserProfileFragment.currentUser.getAccessToken();
+                HttpRequests reviewRequests = new HttpRequests();
+                reviewRequests.execute("reviews", "POST", review.toString(), token);
+                String responseLogin = null;
+                try {
+                    responseLogin = reviewRequests.get();
+                    String responseCode = reviewRequests.getStatus().toString();
+                    System.out.println(responseCode);
+                    populateReviews(findViewById(R.id.review_page));
+
+//                    if (!responseCode.equals("200")) {
+//                        inputStream = reviewRequests.getErrorStream();
+//                    } else {
+//                        inputStream = httpURLConnection.getInputStream();
+//                    }
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+    }
+
 }
+
 
