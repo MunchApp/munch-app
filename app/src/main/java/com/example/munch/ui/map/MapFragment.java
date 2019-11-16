e579a3e2e341c86b80fb0ffc111bf8917808a423
yasirayounus <32212118+yasirayounus@users.noreply.github.com>
2019-11-15 23:25:00 -0600

    added stuff

--- app/src/main/java/com/example/munch/ui/map/MapFragment.java
+++ app/src/main/java/com/example/munch/ui/map/MapFragment.java
@@ -1,72 +1,45 @@
 package com.example.munch.ui.map;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.TranslateAnimation;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.munch.HttpRequests;
 import com.example.munch.LocationCalculator;
 import com.example.munch.R;
 import com.example.munch.data.model.FoodTruck;
-import com.example.munch.ui.foodTruck.CustomInfoWindowAdaptor;
-import com.example.munch.ui.foodTruck.FoodTruckFragment;
-import com.example.munch.ui.more.AboutPageActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelSlideListener;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+public class MapFragment extends Fragment{
 
     private MapViewModel mapViewModel;
     public ImageView firstIm;
-    static GoogleMap munMap;
-    private ArrayList<FoodTruck> listing;
-    static ArrayList<FoodTruck> forWindow;
-
-    private Marker lastClicked;
-
     EditText searchText;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -86,13 +59,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 double lat = locationCalculator.getLat();
                 double lng = locationCalculator.getLng();
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lat, lng)));
-                mMap.animateCamera(CameraUpdateFactory.zoomTo(11.0f));
-                munMap = mMap;
-
-                initialePopulation();
-                munMap.setInfoWindowAdapter(new CustomInfoWindowAdaptor(getContext()));
-                munMap.setOnMarkerClickListener(myMarkerClick);
-                munMap.setOnInfoWindowClickListener(myWindowClick);
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(13.0f));
             }
         });
 
@@ -143,36 +110,36 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         });
         searchText.setClickable(true);
         searchText.setOnClickListener(new View.OnClickListener() {
-            @Override
+           @Override
             public void onClick(View v) {
-                AlphaAnimation fadeOut = new AlphaAnimation(1.0f, 0.0f);
-                fadeOut.setDuration(1000);
-                AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
-                fadeIn.setDuration(1000);
-                fadeIn.setFillAfter(true);
-                ObjectAnimator animation = ObjectAnimator.ofFloat(searchBar, "translationY", -170f);
-                animation.setDuration(1000);
-                animation.start();
-                if(logoText.getVisibility() == View.VISIBLE) {
-                    logoText.setVisibility(View.INVISIBLE);
-                    logoText.startAnimation(fadeOut);
-                }
-                if (locationBar.getVisibility() == View.GONE) {
-                    locationBar.setVisibility(View.VISIBLE);
-                    locationBar.startAnimation(fadeIn);
-                }
-                background.setVisibility(View.VISIBLE);
-                int negHeight = background.getHeight() * -1;
-                TranslateAnimation animate = new TranslateAnimation(
-                        0,
-                        0,
-                        negHeight,
-                        0);
-                animate.setDuration(1000);
-                animate.setFillAfter(true);
-                background.startAnimation(animate);
-                options.setVisibility(View.VISIBLE);
-                options.startAnimation(fadeIn);
+               AlphaAnimation fadeOut = new AlphaAnimation(1.0f, 0.0f);
+               fadeOut.setDuration(1000);
+               AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+               fadeIn.setDuration(1000);
+               fadeIn.setFillAfter(true);
+               ObjectAnimator animation = ObjectAnimator.ofFloat(searchBar, "translationY", -170f);
+               animation.setDuration(1000);
+               animation.start();
+               if(logoText.getVisibility() == View.VISIBLE) {
+                   logoText.setVisibility(View.INVISIBLE);
+                   logoText.startAnimation(fadeOut);
+               }
+               if (locationBar.getVisibility() == View.GONE) {
+                   locationBar.setVisibility(View.VISIBLE);
+                   locationBar.startAnimation(fadeIn);
+               }
+               background.setVisibility(View.VISIBLE);
+               int negHeight = background.getHeight() * -1;
+               TranslateAnimation animate = new TranslateAnimation(
+                       0,
+                       0,
+                       negHeight,
+                       0);
+               animate.setDuration(1000);
+               animate.setFillAfter(true);
+               background.startAnimation(animate);
+               options.setVisibility(View.VISIBLE);
+               options.startAnimation(fadeIn);
             }
         });
 
@@ -213,21 +180,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         return root;
     }
-
-    private GoogleMap.OnMarkerClickListener myMarkerClick = new GoogleMap.OnMarkerClickListener() {
-        @Override
-        public boolean onMarkerClick(Marker markey) {
-            if(lastClicked != null) {
-                lastClicked.setIcon(BitmapDescriptorFromVector(getContext(), R.drawable.ft_dot));
-
-            }
-            markey.showInfoWindow();
-            markey.setIcon(BitmapDescriptorFromVector(getContext(), R.drawable.ft_truck));;
-            lastClicked = markey;
-
-            return true;
-        }
-    };
 
     private void closeSearch (SlidingUpPanelLayout slideUpPanel, TextView logoText,View searchBar,View locationBar, View background, View options){
         AlphaAnimation fadeOut = new AlphaAnimation(1.0f, 0.0f);
@@ -294,196 +246,20 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         SearchListingAdapter mAdapter = new SearchListingAdapter(getActivity(), listings);
         resultsList.setAdapter(mAdapter);
 
-        while (listings.size() < 10) {
 
-        }
-
-        listing = listings;
-        forWindow = listing;
-    }
-
-    private ArrayList<FoodTruck> sampleTrucks() {
-        //creates dummy trucks for map pin testing
-        ArrayList<FoodTruck> testTrucks = new ArrayList<>();
-
-        for (int i = 0; i < 5; i++) {
-            FoodTruck tester = new FoodTruck("identification" + i, "testTruck" + i, 30.2672f + i * 0.1f, -97.7431f - i * .1f);
-            testTrucks.add(tester);
-        }
-        return testTrucks;
-    }
-
-    private void repopulateFromList(ArrayList<FoodTruck> nearby) {
-        //Clears map of pins and populates with new list
-        //place pins on map from ArrayList of trucks
-        munMap.clear();
-        if (nearby != null) {
-            for (int i = 0; i < 3; i++) {
-                populateFromFT(nearby.get(i));
-            }
-        }
-    }
-
-    private void initialePopulation() {
-        //initial Food Truck Population
-        if (listing != null) {
-            for (int i = 0; i < listing.size(); i++) {
-                populateFromFT(listing.get(i));
-
-            }
-        }
-    }
-
-    private FoodTruck getTruckFromMarker(Marker myMark) {
-        //Returns FoodTruck from a marker
-        //Assumes FoodTruck is in listing ArrayList
-        //The arrayList listing cannot be null or crashes
-
-
-        for (int i = 0; i < listing.size(); i++) {
-            if (listing.get(i).getMarker().equals(myMark)) {
-                return listing.get(i);
-            }
-        }
-        return listing.get(0);
-    }
-
-
-    private void populateTrucksFromList(ArrayList<FoodTruck> nearby) {
-        //Todo  input list of trucks output pins on map
-        //place pins on map from ArrayList of trucks
-        if (nearby != null) {
-            for (int i = 0; i < 2; i++) {
-                populateFromFT(nearby.get(i));
-            }
-        }
-    }
-
-
-    private void populateFromFT(FoodTruck truck){
-        //creates new marker for FoodTruck and displays them on map
-        Marker marker = munMap.addMarker(truck.getMarkerOption());
-        truck.setMarker(marker);
-        truck.setVisibilityOn();
-        marker.setIcon(BitmapDescriptorFromVector(getContext(), R.drawable.ft_dot));
-
-        if(truck.getPhotos().size() != 0){
-            marker.setSnippet(truck.getAvgRating() + " " + truck.getPhotos().get(0));
-        }
-        else if(truck.getPhotos().size() == 0){
-            marker.setSnippet(truck.getAvgRating() + " " + "None");
-        }
-    }
-
-    private void hideFTMarker(FoodTruck truck){
-        //hides a specific FoodTrucks marker from the map
-        truck.getMarker().setVisible(false);
-        truck.setVisibilityOff();
-    }
-
-    private void displayFTMarker(FoodTruck truck){
-        //displays a specific FoodTrucks marker on the map
-        truck.getMarker().setVisible(true);
-        truck.setVisibilityOn();
-    }
-
-
-
-    private BitmapDescriptor BitmapDescriptorFromVector(Context context, int vectorResId){
-        Drawable vectorDrawable= ContextCompat.getDrawable(context, vectorResId);
-        vectorDrawable.setBounds(0,0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
-        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
-                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        vectorDrawable.draw(canvas);
-        return BitmapDescriptorFactory.fromBitmap(bitmap);
-    }
-
-
-
-
-
-
-
-//    private void populateTrucksFromTag(ArrayList<FoodTruck> nearby, String tag){
-//        //place pins on map from ArrayList of trucks
-//        for(int i = 0; i < listing.size(); i++){
-//            if(listing.get(i).getTags() == null){
-//                boolean flag = false;
-//                for(int j = 0; j <  listing.get(i).getTags().size(); j++){
-//                      if(listing.get(i).getTags().get(j) == tag){
-//                            flag = true;
-//                      }
-//                if(flag = true){
-//                      displayFTMarker(listing.get(i);
-//                  }
-//                }
+        //TODO create GET request and get the results needed to populate, store in ArrayList of Map
+//        for (int i = 0; i < 5; i++){
 //
-//            }
 //        }
-//            }
-//        }
-//    }
 
-///////////////TRASH    Below
-//    private void populateOfFoodTruck(FoodTruck truck){
-//        populateTruckPin(truck.getLatitude(), truck.getLongitude(), truck.getName(), truck.getId());
-//    }
 
-    private void popFoodTruck(MarkerOptions markO){
-        //May delete
-        munMap.addMarker(markO);
     }
 
 
-    public void populateTruckPin(float lat, float lng, String name, String id){
-        //Todo set create pin from truck info
-        //helper function for populateNearbyTrucks
-
-        Marker marker = munMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(name));
-
-//        marker.set
-//        MarkerOptions markeres = new MarkerOptions().position(new LatLng(30.3152, (-97.0726))).title("TestTruck2");
-////        munMap.addMarker(markeres);
-//        munMap.addMarker(markeres);
-
-//        MarkerOptions markeres = new MarkerOptions().position(new LatLng(lat, lng)).title("TestTruck2");
-////        munMap.addMarker(markeres);
-//        Marker market = munMap.addMarker(markeres);
-
-//        if i set the title as the id, i can find the name, or i can find the info in the text window
-
-//
-    }
-
-    private void populateSearchedTruckPin(float lat, float lng, String name, String id){
-        //Todo set pin for single truck found through searchbar
-        //sets pin for truck found from searching
-//        munMap.clear();
-//        Marker markered =
-                munMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(name));
-//        markered.showInfoWindow();
-//        munMap.setOnMarkerClickListener(onMarkerClickedListener);
-    }
 
 
-    private void populatePins() {
+    private void populatePins(View root) {
         //TODO get pins for existing food trucks to populate initial map
-        munMap.clear();
-        munMap.addMarker(new MarkerOptions().position(new LatLng(37.415229, -122.17265)).title("TestTruck"));
-        munMap.addMarker(new MarkerOptions().position(new LatLng(37.415229, -122.07265)).title("TestTruck2"));
     }
-
-    private GoogleMap.OnInfoWindowClickListener myWindowClick = new GoogleMap.OnInfoWindowClickListener() {
-        @Override
-        public void onInfoWindowClick(Marker markey) {
-            FragmentActivity activity = (FragmentActivity) getContext();
-            FragmentManager fragmentManager = activity.getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            FoodTruckFragment NAME = new FoodTruckFragment(getTruckFromMarker(markey), false);
-            fragmentTransaction.replace(R.id.nav_host_fragment, NAME);
-            fragmentTransaction.commit();
-        }
-    };
 
 }
